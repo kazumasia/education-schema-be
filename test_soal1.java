b962bc16b7d14327647cf5dfcfdbfb5f40cdd0ec
kazumasia <109078511+kazumasia@users.noreply.github.com>
2022-07-12 01:52:31 +0700

    Update test_soal1.java

--- test_soal1.java
+++ test_soal1.java
@@ -1,9 +1,20 @@
-public class test_soal1
-{
-	public static void main(String[] args) {
-		var data = new ArrayList[1, 2, 4, 3, 10, 11, 20, 5, 100, 200];
-        for (var i = 0; i < data.length; i++){
-            Systemout.println(data);
-        }
-	}
-}
+
+public class test_soal1 {
+    
+    public static void main(String[] args) {
+      int[] data = {1, 2, 4, 3, 10, 11, 20, 5, 100, 200};
+      for (int i=0;i<data.length;i++){
+              System.out.println(data[i]);
+              if(i == 5){
+                  System.out.println(data[5] +"fizz");
+              }
+              else if(i == 7){
+                  System.out.println( data[7] + "buzz");
+              }
+              
+               
+    }
+    
+    
+  }
+  }
